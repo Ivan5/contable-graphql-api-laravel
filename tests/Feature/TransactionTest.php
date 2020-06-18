39f4c269dc4f1d5bc95f8ce00a80d02bc55602c7
Ivan5 <converse_ivan@hotmail.com>
2020-06-18 11:30:00 -0500

    Make a test to verify if the balance can rest the amount

--- tests/Feature/TransactionTest.php
+++ tests/Feature/TransactionTest.php
@@ -57,4 +57,49 @@ class TransactionTest extends TestCase
             ]
         ]);
     }
+
+    function test_it_creates_transaction_and_update_balance_with_expense(){
+        //prepare
+        $user = factory(User::class)->create();
+        $account = factory(Account::class)->create([
+            'user_id' => $user->id,
+            'balance' => 100
+        ]);
+        Passport::actingAs($user);
+        //execute
+        $response = $this->graphQL('
+            mutation{
+                createTransaction(input:{
+                    account_id:'.$account->id.',
+                    type: EXPENSE,
+                    description:"Expense",
+                    amount: 50
+                }){
+                    description
+                    type
+                    amount
+                    account{
+                        id
+                        name
+                        balance
+                    }
+                }
+            }
+        ');
+        //assert
+        $response->assertJson([
+            'data' => [
+               'createTransaction' => [
+                   'description' => 'Expense',
+                   'type'=>'EXPENSE',
+                   'amount'=>50.00,
+                   'account' => [
+                       'id' => $account->id,
+                       'name' => $account->name,
+                       'balance' => 50.00
+                   ]
+               ] 
+            ]
+        ]);
+    }
 }
